046bab3a6d72df50c94895e33b1b2f2c6b538c3c
apoliko <64897230+apoliko@users.noreply.github.com>
2026-03-04 16:23:22 +0300

    an example of higher order functions from lecture

--- lecture 2/lectureCodes.dart
+++ lecture 2/lectureCodes.dart
@@ -21,6 +21,31 @@ class Collection<T>{
 void sayHello(String name) => print("Hello, $name!");
 
 void printMap(String key, int value) => print("$key : $value");
+Function makeLoginCounter(){
+  int count = 0;
+  return (){
+    //Login logic goes here!
+    print("Logging in...");
+    count++;
+    return count;
+  }; //Captures count
+}
+class Post{
+  String username;
+  String content;
+  Post(this.username, this.content);
+  @override
+  String toString() => "$username: $content";
+}
+List<Post> filterPost(List<Post> posts, bool Function(Post) filterFunc){
+  List<Post> filtered = [];
+  for(var post in posts){
+    if(filterFunc(post)){
+      filtered.add(post);
+    }
+  } 
+  return filtered;
+}
 void main(){
   var collection = Collection<Student>("Students",[
     Student("Ali","101"),
@@ -41,4 +66,19 @@ void main(){
   grades.forEach((String key, int value) => print("$key : $value"));
   grades.forEach(printMap);
 
+
+  var counter = makeLoginCounter();
+  print(counter());
+  print(counter());
+  print(counter());
+
+
+  var posts = [
+    Post("alice", "Loving dart"),
+    Post("bob", "Hello world!"),
+    Post("hank", "Async coding"),
+    Post("david", "Goodbye!")
+  ];
+  var filtered = filterPost(posts, (Post post) =>post.username == "alice" || post.username == "bob");
+  print(filtered);
 }
