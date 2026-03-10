a088d670511a7cd091356f485201f5b032dfcda7
apoliko <64897230+apoliko@users.noreply.github.com>
2026-03-10 21:23:03 +0300

    some practices and Widgets

--- lecture 3/my_first_app/lib/main.dart
+++ lecture 3/my_first_app/lib/main.dart
@@ -5,6 +5,27 @@
 //?   we can see our app in chrome
 //&   also by using "flutter devices" you can see possible devices you can use to your app
 
+
+//! CORE WIDGETS
+//* Text("Hello World!", style: TextStyle(fontSize: 20,color: Color.fromARGB(255, 21, 0, 255))
+//* Image.network('https://...')
+//* ElevatedButton(onPressed: (){}, child: Text('Click me!'))
+//* Icon(Icon.star, color: Colors.amber, size: 40)
+
+//! Scaffold & Layout Widgets
+//* Scaffold: App skeleton
+//* Container: Styling box
+//* Column: Vertical layout
+//* Row: horizontal layout
+
+//& Some other core widgets
+//* appBar: Top bar with title, back button, or actions.
+//* body: The main content area.
+//* floatingActionButton: A button that floats above the UI
+//* drawer: A slide-out side menu.
+//* bottomNavigationBar: Tab bar at the bottom.
+
+
 import 'package:flutter/material.dart';
 void main(){
   runApp(const MyApp());
@@ -15,9 +36,23 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       home: Scaffold(
-        body: Center(
-          child: Text("Hello World!"),
-        ),
+        body: Column(
+          mainAxisAlignment: MainAxisAlignment.center,    // Contorls vertical alignment (e.g, start, center, spaceBetween)
+          crossAxisAlignment:  CrossAxisAlignment.start,  // Control horizontal alignment (e.g, start, center ,stretch)
+          children: [                                     // List of widgets to display
+            Text("Item 1"),
+            Text("Item 2"),
+            Text("Item 3"),
+            Row(
+              mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+              children: [
+                Icon(Icons.star, color: Colors.amber),
+                ElevatedButton(onPressed: (){print("Button pressed! ");}, child: Text("Get Started")),
+                Icon(Icons.star, color: Colors.amber,) 
+                ]
+            )
+          ],
+        )
       ),
     );
   }
