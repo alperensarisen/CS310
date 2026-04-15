6f8d848b69de922cb641c3cb755d1fee1ea78beb
apoliko <64897230+apoliko@users.noreply.github.com>
2026-04-15 03:52:14 +0300

    shared preferences usage

--- lecture 3/my_first_app/lib/main.dart
+++ lecture 3/my_first_app/lib/main.dart
@@ -1,59 +1,66 @@
 import 'package:flutter/material.dart';
 import 'package:shared_preferences/shared_preferences.dart';
-
-void main() => runApp(MaterialApp(home: HomePage()));
-
-class HomePage extends StatefulWidget {
-  const HomePage({super.key});
+void main() => runApp(MaterialApp(home: homePage(),));
+class homePage extends StatefulWidget {
+  const homePage({super.key});
 
   @override
-  State<HomePage> createState() => _HomePageState();
+  State<homePage> createState() => _homePageState();
 }
 
-class _HomePageState extends State<HomePage> {
-  String? username;
+class _homePageState extends State<homePage> {
+  final _contorller = TextEditingController();
+  List<String> names = ["alperen", "egemen"];
   @override
   void initState(){
     loadData();
     super.initState();
   }
-  void saveData() async{
-    final pref = await SharedPreferences.getInstance();
-    await pref.setString('name', _controller.text);
+
+  void saveData()async{
+    final prefs = await SharedPreferences.getInstance();
+    await prefs.setStringList('data', [...names, _contorller.text]);
     setState(() {
-      username = _controller.text;
+      names = [...names, _contorller.text];
     });
   }
   void loadData() async{
-    final pref = await SharedPreferences.getInstance();
-    setState((){
-      if(pref.getString('name') == null){
-        username = 'No named saved';
-      }
-      else{
-        username = pref.getString('name');
-      }
-    }
-    );
+    final prefs = await SharedPreferences.getInstance();
+    setState(() {
+      names = prefs.getStringList('data') ?? [];
+    });
   }
-  final _controller = TextEditingController();
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(title: Text(username!),),
-      body: Column(
-        children: [
-          TextField(
-            controller: _controller,
-            decoration: InputDecoration(
-              label: Text("Enter Username"),
-              border: OutlineInputBorder(),
-              suffix: IconButton(onPressed: (){_controller.clear();}, icon: Icon(Icons.clear)),
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            TextField(
+              controller: _contorller,
+              decoration: InputDecoration(
+                label: Text("Name"),
+                border: OutlineInputBorder(),
+                suffix: IconButton(onPressed: (){_contorller.clear();}, icon: Icon(Icons.clear))
+              ),
             ),
-            
-          ),
-          ElevatedButton(onPressed: (){saveData();}, child: Text("Save"))
-        ],
+            SizedBox(height: 10,),
+            ElevatedButton(onPressed: (){saveData();}, child: Text("Add")),
+            SizedBox(height: 10,),
+            Expanded(
+              child: ListView.builder(
+                itemCount: names.length,
+                itemBuilder: (BuildContext context, int index){
+                  final name = names[index];
+                  return ListTile(
+                    leading: Icon(Icons.save),
+                    title: Text(name),
+                  );
+                }),
+            )
+          ],
+        ),
       ),
     );
   }
