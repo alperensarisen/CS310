6d04a19c31cbad0bce1ea061702446e702259ce1
apoliko <64897230+apoliko@users.noreply.github.com>
2026-05-10 00:17:59 +0300

    update

---
+++ lecture 3/my_first_app/lib/models/book.dart
@@ -0,0 +1,20 @@
+class Book {
+ final int id;
+ final String title;
+ final String author;
+ final int year;
+ final String genre;
+ final bool bestseller;
+ final double rating;
+ final String description;
+ Book({
+ required this.id,
+ required this.title,
+ required this.author,
+ required this.year,
+ required this.genre,
+ required this.bestseller,
+ required this.rating,
+ required this.description,
+ });
+}
